071566ae805da8d0a6426215860a14f8e686e7fe
jlPortus <jl_puerto@hotmail.com>
2023-03-10 13:09:13 +0100

    eliminados test

--- src/main/java/com/example/lib/LibApplication.java
+++ src/main/java/com/example/lib/LibApplication.java
@@ -1,8 +1,5 @@
 package com.example.lib;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 //@SpringBootApplication
 public class LibApplication {
 
